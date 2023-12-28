828b0d29b6df4b94de749a48872e0e9c3a03f3b7
Karl Fredrik Haugland <kfh@tla.wtf>
2023-12-28 02:04:17 +0100

    Handle long lines better

--- cfp/__init__.py
+++ cfp/__init__.py
@@ -5,6 +5,7 @@ import toml
 from escpos.printer import Serial
 
 
+WIDTH = 42
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 FORTUNE_FILE = os.path.join(CURRENT_DIR, "fortunes.txt")
 # Lazy dir handling...
@@ -38,7 +39,28 @@ def _get_fortune():
         data = f.read()
     fortunes = data.split("\n-\n")
     fortune = fortunes[randint(0, len(fortunes)-1)]
-    return fortune
+    return _fix_line_maxlength(fortune)
+
+def _fix_line_maxlength(text):
+    """
+    text = "The quick brown fox jumped over the lazy dog\nFinito"
+    parts = [
+        "The quick brown fox jumped over the lazy dog",
+        "Finito"
+    ]
+    """
+    ret = []
+    for part in text.split("\n"):
+        if len(part) > WIDTH:
+            ret = ret or [""]
+            for word in part.split(" "):
+                if len(f"{ret[-1]} {word}") > WIDTH:
+                    ret.append("")
+                ret[-1] = f"{ret[-1]} {word}"
+        else:
+            ret.append(part)
+    return "\n".join(ret)
+
 
 
 def _setup_printer():
